304aadbb5506acb1c7b8de51e53086a071d760c6
이경창 <lkc263@naver.com>
2023-08-26 17:48:40 +0900

    행렬 문제 풀기

---
+++ 이경창/2023/0826/행렬/Solution3.java
@@ -0,0 +1,18 @@
+package 행렬;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = {{1, 2}};
+        int[][] arr2 = {{1, 2}};
+
+        
+
+
+        reader.close();
+    }
+}
