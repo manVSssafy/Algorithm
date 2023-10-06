236d03485dc866c00831ac9ce3b0137e6ce29041
이경창 <lkc263@naver.com>
2023-10-06 19:04:23 +0900

    A와 B 2

---
+++ 이경창/2023/1006/A와B2/Solution2.java
@@ -0,0 +1,38 @@
+package A와B2;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution2 {
+
+    private static String S, T;
+    private static int answer;
+    private static void dfs(StringBuilder builder){
+        
+        if(builder.length() <= 0) return;
+        if(builder.length() == S.length()){
+            if(builder.toString().equals(S)) answer = 1;
+            return;
+        }
+
+        if(builder.charAt(0) == 'B'){
+            StringBuilder nextBuilder = new StringBuilder(builder);
+            dfs(new StringBuilder(nextBuilder.reverse().deleteCharAt(nextBuilder.length() - 1)));
+        }
+        if(builder.charAt(builder.length() - 1) == 'A') dfs(new StringBuilder(builder.deleteCharAt(builder.length() - 1)));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        S = reader.readLine();
+        T = reader.readLine();
+
+        dfs(new StringBuilder(T));
+
+        System.out.println(answer);
+
+
+        reader.close();
+    }
+}
