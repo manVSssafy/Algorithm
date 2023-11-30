a25d8fe3b806b22ea55a5959c1de2597228b90f6
이경창 <lkc263@naver.com>
2023-11-30 17:09:14 +0900

    빌런 호석

---
+++ 이경창/2023/1130/빌런_호석/Test.java
@@ -0,0 +1,9 @@
+package 빌런_호석;
+
+public class Test {
+    public static void main(String[] args) {
+        int number = 1023;
+        String s = String.valueOf(number);
+        System.out.println(s);
+    }
+}
